ac4bea3fc352aab1c32a85a2efe337db9de3ece8
amateurtechguy <ziadzizo192010@gmail.com>
2026-08-21 18:09:07 +0100

    added level screen

--- Script/Global.gd
+++ Script/Global.gd
@@ -1,5 +1,7 @@
-extends Node2D
+extends Node
 
+var minigames_done = 0 
+var lives = 5
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,11 +11,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/level_scene.tscn")
-
-
-func _on_quit_pressed() -> void:
-	get_tree().quit()
