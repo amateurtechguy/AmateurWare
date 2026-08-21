7b8307581381cf64b96a5fb2ebf83cba65805b81
amateurtechguy <ziadzizo192010@gmail.com>
2026-08-21 21:44:43 +0100

    fixed bugs and debugged

--- Scenes/node_2d.gd
+++ Scenes/node_2d.gd
@@ -11,7 +11,7 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_button_pressed() -> void:
+func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/level_scene.tscn")
 
 
